1397bb8ca5c0adad2e5aaeaf8da8347c3ec45a45
Kirk George <kirk.e.george@gmail.com>
2020-05-26 17:22:04 -0700

    KEG Added promises.  Started namespace enforcement--will require update

--- Assets/Utils/Runtime/src/HashMap.cs
+++ Assets/Utils/Runtime/src/HashMap.cs
@@ -1,108 +1,111 @@
 ﻿
 using Stack = System.Collections.Generic.Stack<int>;
 
-public class HashMap<T>
+namespace com.keg.utils
 {
-    private T[] _map;
-    private int _mapSize;
-
-    private Stack _freedIds;
-    private int _nextId;
-
-    public HashMap( int size = 10 )
+    public class HashMap<T>
     {
-        _mapSize = 10;
-        _nextId = 0;
-        _freedIds = new Stack( _mapSize );
+        private T[] _map;
+        private int _mapSize;
 
-        CreateMap();
-    }
+        private Stack _freedIds;
+        private int _nextId;
 
-    private void ResizeMap()
-    {
-        _mapSize *= 2;
-    }
-
-    private void CreateMap()
-    {
-        int count = _map != null ? _map.Length : 0;
-        T[] newMap = new T[ _mapSize ];
-
-        for( int i = 0; i < count; ++i )
+        public HashMap( int size = 10 )
         {
-            newMap[ i ] = _map[ i ];
+            _mapSize = 10;
+            _nextId = 0;
+            _freedIds = new Stack( _mapSize );
+
+            CreateMap();
         }
 
-        _map = newMap;
-    }
-
-    public int GetNextId()
-    {
-        if( IsNextIdRecycled() )
+        private void ResizeMap()
         {
-            return _freedIds.Peek();
-        }
-        else
-        {
-            return _nextId;
-        }
-    }
-
-    public bool IsNextIdRecycled()
-    {
-        return _freedIds.Count > 0;
-    }
-
-    public int Add( T thing )
-    {
-        bool isIdRecycled = IsNextIdRecycled();
-        int id = isIdRecycled ? _freedIds.Peek() : _nextId;
-
-        if( id >= _map.Length )
-        {
-            ResizeMap();
+            _mapSize *= 2;
         }
 
-        _map[ id ] = thing;
-
-        if( isIdRecycled )
+        private void CreateMap()
         {
-            _freedIds.Pop();
-        }
-        else
-        {
-            _nextId++;
-        }
+            int count = _map != null ? _map.Length : 0;
+            T[] newMap = new T[ _mapSize ];
 
-        return id;
-    }
-
-    public void Remove( int id )
-    {
-        if( IsIdValid( id ) )
-        {
-            _freedIds.Push( id );
-            _map[ id ] = default( T );
-        }
-    }
-
-    public T this[ int id ]
-    {
-        get
-        {
-            if( !IsIdValid( id ) )
+            for( int i = 0; i < count; ++i )
             {
-                return default( T );
+                newMap[ i ] = _map[ i ];
+            }
+
+            _map = newMap;
+        }
+
+        public int GetNextId()
+        {
+            if( IsNextIdRecycled() )
+            {
+                return _freedIds.Peek();
             }
             else
             {
-                return _map[ id ];
+                return _nextId;
             }
         }
-    }
 
-    private bool IsIdValid( int id )
-    {
-        return id < _map.Length && id < _nextId;
+        public bool IsNextIdRecycled()
+        {
+            return _freedIds.Count > 0;
+        }
+
+        public int Add( T thing )
+        {
+            bool isIdRecycled = IsNextIdRecycled();
+            int id = isIdRecycled ? _freedIds.Peek() : _nextId;
+
+            if( id >= _map.Length )
+            {
+                ResizeMap();
+            }
+
+            _map[ id ] = thing;
+
+            if( isIdRecycled )
+            {
+                _freedIds.Pop();
+            }
+            else
+            {
+                _nextId++;
+            }
+
+            return id;
+        }
+
+        public void Remove( int id )
+        {
+            if( IsIdValid( id ) )
+            {
+                _freedIds.Push( id );
+                _map[ id ] = default( T );
+            }
+        }
+
+        public T this[ int id ]
+        {
+            get
+            {
+                if( !IsIdValid( id ) )
+                {
+                    return default( T );
+                }
+                else
+                {
+                    return _map[ id ];
+                }
+            }
+        }
+
+        private bool IsIdValid( int id )
+        {
+            return id < _map.Length && id < _nextId;
+        }
     }
 }
